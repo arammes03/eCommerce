bbed16d37adcc47393098852a3a9846dcaf0704a
arammes03 <arammes03@gmail.com>
2024-01-24 22:17:05 +0100

    ciclos de vida de componentes: ngOnChanges

---
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css',
+})
+export class CounterComponent {
+  @Input({ required: true }) duration = 0;
+  @Input({ required: true }) message = '';
+
+  constructor() {
+    // NO ASYNC
+    // antes de render
+    console.log('contructor');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    // before and during render
+    console.log('ngOnChanges');
+    console.log('-'.repeat(10));
+    console.log(changes);
+  }
+}
