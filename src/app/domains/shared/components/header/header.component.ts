8e1aa947c8d39b19fb552bb97b21eb124b04d801
arammes03 <arammes03@gmail.com>
2024-01-25 21:02:24 +0100

    Menú desplegable del carrito

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  hideSideMenu = signal(true); // Señal que guarda el estado del menú (visible/no visible)
 
+  // Función que cambiará el estado de la señal
+  toogleSideMenu() {
+    this.hideSideMenu.update((prevState) => !prevState);
+  }
 }
