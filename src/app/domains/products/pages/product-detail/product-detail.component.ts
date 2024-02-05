a5498655a6481613c4e4425ec6f1d059c4194a5c
Alfonso Ramírez <arammes03@gmail.com>
2024-02-05 17:25:50 +0100

    mostrar detalles del producto y boton de añadir al carrito desde un producto

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,28 +1,45 @@
 import { Component, Input, inject, signal } from '@angular/core';
 import { Product } from '@shared/models/product.model';
 import { ProductService } from '@shared/services/product.service';
-import { CurrencyPipe } from '@angular/common';
+import { CurrencyPipe, UpperCasePipe } from '@angular/common';
+import { CartService } from '@shared/services/cart.service';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, UpperCasePipe],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css',
 })
 export class ProductDetailComponent {
   @Input() id?: string;
   product = signal<Product | null>(null);
+  coverImg = signal('');
 
   private productService = inject(ProductService);
+  private cartService = inject(CartService)
 
   ngOnInit() {
     if (this.id) {
       this.productService.getProduct(this.id).subscribe({
         next: (product) => {
           this.product.set(product);
+          if (product.images.length > 0) {
+            this.coverImg.set(product.images[0])
+          } 
         },
       });
     }
+  }
+
+  // Función que cambia la foto mostrada en grande
+  changeCover(newImg: string) {
+    this.coverImg.set(newImg);
+  }
+
+  addToCart() {
+    const product = this.product();
+    if (product) 
+      this.cartService.addToCart(product);
   }
 }
