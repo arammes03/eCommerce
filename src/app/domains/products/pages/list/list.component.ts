f54ca06cac6eab0e4375f952862cd0dc3468c4d0
Alfonso Ramírez <arammes03@gmail.com>
2024-02-05 18:10:39 +0100

    mostrando diferentes categorias y filtrado

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,33 +1,55 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, Input, SimpleChanges, inject, signal } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
 
 //Import componentes
 import { ProductComponent } from '@products/components/product/product.component';
 import { HeaderComponent } from '@shared/components/header/header.component';
+import { Category } from '@shared/models/category.model';
 
 // Import modelos
 import { Product } from '@shared/models/product.model';
 
 // Import servicios
 import { CartService } from '@shared/services/cart.service';
+import { CategoryService } from '@shared/services/category.service';
 import { ProductService } from '@shared/services/product.service';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [ProductComponent, HeaderComponent],
+  imports: [ProductComponent, HeaderComponent, RouterLinkWithHref],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css',
 })
 export class ListComponent {
   // Signals
   products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
 
   // Servicios
   private cartService = inject(CartService);
   private productService = inject(ProductService);
+  private categoryService = inject(CategoryService);
+
+  // Inputs
+  @Input() category?: string;
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.getProducts();
+  }
 
   ngOnInit() {
-    this.productService.getProducts().subscribe({
+    this.getCategories();
+  }
+
+  // Función que nos agrega un producto al carrito
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+  }
+
+  private getProducts() {
+    this.productService.getProducts(this.category)
+    .subscribe({
       next: (products) => {
         this.products.set(products);
       },
@@ -37,8 +59,14 @@ export class ListComponent {
     });
   }
 
-  // Función que nos agrega un producto al carrito
-  addToCart(product: Product) {
-    this.cartService.addToCart(product);
+  private getCategories() {
+    this.categoryService.getAllCategories().subscribe({
+      next: (data) => {
+        this.categories.set(data);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
   }
 }
--- src/app/domains/shared/services/product.service.ts
+++ src/app/domains/shared/services/product.service.ts
@@ -8,8 +8,11 @@ import { Product } from '../models/product.model';
 export class ProductService {
   private url = inject(HttpClient); // Permite hacer requests a http
 
-  getProducts() {
-    return this.url.get<Product[]>('https://api.escuelajs.co/api/v1/products');
+  getProducts(category?: string) {
+    const url = new URL('https://api.escuelajs.co/api/v1/products');
+    if (category)
+      url.searchParams.set('categoryId', category);
+    return this.url.get<Product[]>(url.toString());
   }
 
   getProduct(id: string) {
